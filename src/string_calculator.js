59c533df127abf90f4387c706d2e5e17b16872cd
mesha <mesha@KATIJA>
2026-05-25 21:43:38 +0200

    3: driver amelie 3

--- src/string_calculator.js
+++ src/string_calculator.js
@@ -2,13 +2,13 @@
 function StringCalculator() {}
 StringCalculator.prototype.add = function(string_numbers) {
 
-	let value=0; 
+	let value = 0;
+
     if(string_numbers.includes('3')){
 	    value = 2;
     }
 	if(string_numbers.includes('5')){
-	    value += 4;
+	    value = value + 4;
     }
-   
     return value;
 };
